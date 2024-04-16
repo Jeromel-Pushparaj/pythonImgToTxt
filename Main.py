7fd945a217be6d0dd99439725aecf5684c1b2d0f
Jeromel Pushparaj <jeromal03@gmail.com>
2024-04-16 21:06:00 +0530

    I wrote the programand extracted it

---
+++ Main.py
@@ -0,0 +1,11 @@
+# pip install easyocr --user 
+import easyocr
+
+# Initialize EasyOCR with the desired language(s)
+reader = easyocr.Reader(['en'])
+
+# Read text from an image file
+result = reader.readtext('cimg.jpg')
+# Print the extracted text
+for detection in result:
+    print(detection[1])
