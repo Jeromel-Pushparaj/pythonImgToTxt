cedb76eeec7b376911be5af3853fddb49bf332cc
Jeromel Pushparaj <jeromal03@gmail.com>
2024-04-17 20:17:22 +0530

    todays work in this proj

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import easyocr
 reader = easyocr.Reader(['en'])
 
 # Read text from an image file
-result = reader.readtext('cimg.jpg')
+result = reader.readtext('imgsForTesting/nycilpowerincredient.jpg')
 # Print the extracted text
 for detection in result:
     print(detection[1])
